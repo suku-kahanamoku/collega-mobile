f9516a2c6833f9eac3867cecab95517fbe0c4492
Suche Kahanamoku <sukusovi@gmail.com>
2025-02-26 12:07:29 +0100

    Implement localization

--- app/(pages)/index.tsx
+++ app/(pages)/index.tsx
@@ -1,13 +1,16 @@
 import { StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useTranslation } from "react-i18next";
 
 import { ViewCmp, TextCmp } from "@/components/Themed";
 
 export default function TabOneScreen() {
+  const { t } = useTranslation('translation');
+
   return (
     <LinearGradient colors={["#333", "#000"]} style={styles.wrapper}>
       <ViewCmp style={styles.container}>
-        <TextCmp style={styles.title}>Home page</TextCmp>
+        <TextCmp style={styles.title}>{t("hello")}</TextCmp>
       </ViewCmp>
     </LinearGradient>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   DarkTheme,
   DefaultTheme,
@@ -6,69 +6,92 @@ import {
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { I18nextProvider } from "react-i18next";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { useMenus } from "@/hooks/useMenus";
 import DrawerCmp from "@/components/Drawer";
 import ThemeToggleCmp from "@/components/ThemeToggle";
 import HamburgerCmp from "@/components/Hamburger";
 import LogoCmp from "@/components/Logo";
+import LangSwitchCmp from "@/components/LangSwitch";
+import i18n from "../lang/i18n";
 
 export default function RootLayout() {
   const [theme, setTheme] = useState("");
   const [open, setOpen] = useState(false);
   const { menus, activeMenu } = useMenus();
 
-  return (
-    <ThemeProvider value={theme === "dark" ? DarkTheme : DefaultTheme}>
-      <StatusBar barStyle={"default"} />
+  useEffect(() => {
+    const loadLanguage = async () => {
+      try {
+        const storedLanguage = await AsyncStorage.getItem("LANGUAGE");
+        if (storedLanguage) {
+          i18n.changeLanguage(storedLanguage);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    loadLanguage();
+  }, []);
 
-      <DrawerCmp open={open} setOpen={setOpen} menus={menus}>
-        <Stack>
-          <Stack.Screen
-            name="(pages)"
-            options={{
-              headerLeft: LogoCmp,
-              headerRight: () => (
-                <>
-                  <ThemeToggleCmp setTheme={setTheme} />
-                  <HamburgerCmp setOpen={setOpen} />
-                </>
-              ),
-              headerTitle: activeMenu.title,
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="modal"
-            options={{
-              presentation: "modal",
-              headerLeft: LogoCmp,
-              headerRight: () => (
-                <>
-                  <ThemeToggleCmp setTheme={setTheme} />
-                  <HamburgerCmp setOpen={setOpen} />
-                </>
-              ),
-              headerTitle: "Modal",
-              headerTitleAlign: "center",
-            }}
-          />
-          <Stack.Screen
-            name="+not-found"
-            options={{
-              headerLeft: LogoCmp,
-              headerRight: () => (
-                <>
-                  <ThemeToggleCmp setTheme={setTheme} />
-                  <HamburgerCmp setOpen={setOpen} />
-                </>
-              ),
-              headerTitle: "Page not found",
-              headerTitleAlign: "center",
-            }}
-          />
-        </Stack>
-      </DrawerCmp>
-    </ThemeProvider>
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider value={theme === "dark" ? DarkTheme : DefaultTheme}>
+        <StatusBar barStyle={"default"} />
+
+        <DrawerCmp open={open} setOpen={setOpen} menus={menus}>
+          <Stack>
+            <Stack.Screen
+              name="(pages)"
+              options={{
+                headerLeft: LogoCmp,
+                headerRight: () => (
+                  <>
+                    <ThemeToggleCmp setTheme={setTheme} />
+                    <LangSwitchCmp />
+                    <HamburgerCmp setOpen={setOpen} />
+                  </>
+                ),
+                headerTitle: activeMenu.title,
+                headerTitleAlign: "center",
+              }}
+            />
+            <Stack.Screen
+              name="modal"
+              options={{
+                presentation: "modal",
+                headerLeft: LogoCmp,
+                headerRight: () => (
+                  <>
+                    <ThemeToggleCmp setTheme={setTheme} />
+                    <LangSwitchCmp />
+                    <HamburgerCmp setOpen={setOpen} />
+                  </>
+                ),
+                headerTitle: "Modal",
+                headerTitleAlign: "center",
+              }}
+            />
+            <Stack.Screen
+              name="+not-found"
+              options={{
+                headerLeft: LogoCmp,
+                headerRight: () => (
+                  <>
+                    <ThemeToggleCmp setTheme={setTheme} />
+                    <LangSwitchCmp />
+                    <HamburgerCmp setOpen={setOpen} />
+                  </>
+                ),
+                headerTitle: "Page not found",
+                headerTitleAlign: "center",
+              }}
+            />
+          </Stack>
+        </DrawerCmp>
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
