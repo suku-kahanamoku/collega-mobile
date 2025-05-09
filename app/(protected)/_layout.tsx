8adffa7a6143df79e89ef58e24e75c48aeba1275
Suche Kahanamoku <sukusovi@gmail.com>
2025-05-09 09:54:56 +0200

    Add settings to non protectec.

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -2,34 +2,33 @@ import { Redirect, Stack } from "expo-router";
 
 import { useAuth } from "@/modules/Auth/hooks/useAuth";
 import { useRoute } from "@/hooks/useRoute";
+import { useLang } from "@/modules/Lang/hooks/useLang";
 
 export default function PagesLayout() {
+  const { t } = useLang();
   const { session } = useAuth();
   const { menuList, menus } = useRoute();
-
-  if (!session) {
-    return <Redirect href={menuList.login.href} />;
-  }
+  const settingsMenu = menuList.settings;
 
   return (
-    <Stack>
-      {menus.map((menu, index) => (
-        <Stack.Screen
-          key={index}
-          name={menu.name}
-          options={{
-            headerShown: false,
-          }}
-        />
-      ))}
-
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* settings screen */}
       <Stack.Screen
-        name={menuList.settings.name}
+        name={settingsMenu.name}
         options={{
-          headerTitle: session?.user?.name,
-          presentation: "modal",
+          headerTitle: t(settingsMenu.title),
+          presentation: "formSheet",
         }}
       />
+
+      {/* protected screens */}
+      {session ? (
+        menus.map((menu, index) => (
+          <Stack.Screen key={index} name={menu.name} />
+        ))
+      ) : (
+        <Redirect href={menuList.login.href} />
+      )}
     </Stack>
   );
 }
